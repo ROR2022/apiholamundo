7be51febdf769d087bd8f09ac6b87dc65f300630
ROR2022 <rami.ror279@gmail.com>
2022-06-11 15:09:54 -0500

    primeras pruebas pagos

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
 'use strict'
 
-var express = require('express'),
-	app = express()
+const express = require('express');
+const Stripe = require('stripe'),
+		app = express()
+
+const stripe = new Stripe("sk_test_51L8EuNHKsPF0nmZbf6JHakNVUHQFuNYAhBEnmqRUzsQUSYbfsZMMMZV5pTWs3RnQDju8aDYyS6RiUu6VFavTL4BG00PH263eg6");
+
+
+app.use(express.json())
+
+app.post('/api/checkout', async (req,res)=>{
+    //console.log(req.body);
+    const {id, amount} = req.body;
+    try
+    {
+        const payment = await stripe.paymentIntents.create({
+            amount,
+            currency: "USD",
+            description: "pruebas de Node",
+            payment_method: id,
+            confirm: true
+        })
+        console.log(payment);
+        res.send({message: 'Succesfull payment'});
+        
+    }catch (error)
+    {
+        //console.log(error);
+        res.json({message: error.raw.message});
+    }
+})
+
+
 
 app
 	.get('/', (req, res) => {
